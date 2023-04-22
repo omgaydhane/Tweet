4033cea5cda0a91879459ad463bebd71742539ed
omgaydhane <gaydhaneom321@gmail.com>
2023-04-22 19:30:28 +0530

    changes made to tobetried.py

--- tobetried.py
+++ tobetried.py
@@ -5,10 +5,10 @@ import tweepy
 import uuid
 
 # Define your authentication credentials
-consumer_key = '6ZYZTRKthMO6dOnkKW9Ch59H7'
-consumer_secret = '9BrFHs0qibGxtI30LPIp6oI6GUr4txS8EktkkJMU1D3GCZBeIm'
-access_token = '985487935427436544-ZwhoQgUIRk1swlnEkHeOjd6GAZtZ1a4'
-access_token_secret = 'XWg2B9ycb0w7d7rjUkwk5DrvtwBcUXHDyaBrBr58L5DKT'
+consumer_key = 'your-consumer-key'
+consumer_secret = 'your-consumer_secret-key'
+access_token = 'your-access_token'
+access_token_secret = 'your-access_token_secret'
 
 # Authenticate with the Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -25,21 +25,21 @@ num_tweets = 50
 tweets = tweepy.Cursor(api.search_tweets, q=hashtag).items(num_tweets)
 
 # Define your delivery stream name and region
-delivery_stream_name = 'twitter-data'
+delivery_stream_name = 'your-delivery-stream'
 region = 'ap-south-1'
 
 # Create the Kinesis Firehose client
 firehose_client = boto3.client('firehose', region_name='ap-south-1')
 
 # Create a CSV file and write the retrieved tweets to it
-with open('tweets2.csv', mode='w', newline='') as file:
+with open('tweets.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['ID', 'Timestamp', 'Text', 'Username', 'Hashtags', 'Location', 'Followers', 'Retweets', 'Favorites'])
     for tweet in tweets:
         writer.writerow([tweet.id_str, tweet.created_at, tweet.text, tweet.user.screen_name, tweet.entities['hashtags'], tweet.user.location, tweet.user.followers_count, tweet.retweet_count, tweet.favorite_count])
 
 # Send the CSV file to the Kinesis Data Firehose delivery stream
-with open('tweets2.csv', 'rb') as f:
+with open('tweets.csv', 'rb') as f:
     data = f.read()
     response = firehose_client.put_record(
         DeliveryStreamName=delivery_stream_name,
